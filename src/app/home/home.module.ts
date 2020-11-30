600f30871e07e8848b6667a31142be30da6b4a37
welove-t <tlove.tky@gmail.com>
2020-12-01 08:26:43 +0900

    feat(exp-pipe): 経験値表示パイプ追加

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,10 +5,11 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { PetComponent } from './pet/pet.component';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { ExpPipe } from '../exp.pipe';
 
 
 @NgModule({
-  declarations: [HomeComponent, PetComponent],
+  declarations: [HomeComponent, PetComponent, ExpPipe],
   imports: [
     CommonModule,
     HomeRoutingModule,
